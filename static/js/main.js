1279e6a19d00b9c076eaf3863494c552162e291e
tofof <github@tofof.com>
2018-07-23 19:48:22 -0500

    Live version as of 2018
    Catchup to changes to date

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,28 @@
-jQuery.fn.autohidebacktotop = function() {
-	if ($("html, body").height() <= $(window).height()) {
-		$("#autohidebacktotop").remove();
-	}
-	this.bind("click", function() {
-		$("html, body").animate({scrollTop:0});
-		return false;
-	});
-};
-
 jQuery(document).ready(function($) {
 	// Auto-handle direct jpeg links
-	$('a[href$="jpg"]').each(function() {
-        $(this).colorbox({
-            maxWidth: '100%'
+	// $('a[href$="jpg"]').each(function() {
+ //        $(this).colorbox({
+ //            maxWidth: '100%',
+ //            maxHeight: '100%'
+ //        });
+	// });
+
+    //Auto handle direct image links, grouping per paragraph
+    $('p').each(function(index) {
+        $(this).find('a[href$="jpg"],a[href$="jpeg"],a[href$="png"],a[href$="gif"],a[href$="tif"]').colorbox({
+            maxWidth: '100%',
+            maxHeight: '100%',
+            rel: 'group1'.concat(index)            
         });
-	});
-
-    $("#back-to-top").autohidebacktotop();
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop()) {
-            $('#back-to-top:hidden').stop(true, true).fadeIn();
-        } else {
-            $('#back-to-top').stop(true, true).fadeOut();
-        }
     });
+
+    //.css('border', '2px solid red');			//useful to see what it's finding
+
+    //center paragraph contents of paragraphs that have images in them
+    // $("p:has(img)").css("text-align", "center");
+    // $("p:has(img)").css("-moz-text-align-last", "center");
+    // $("p:has(img)").css("text-align-last", "center");
+    
+    
+
 });
-
-
